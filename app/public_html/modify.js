0fb2bf748413067cad3ea564b38ea7629945f952
Kobe Uytiepo <kobeuyt@gmail.com>
2020-09-04 18:04:08 -0400

    added teams getForm

--- app/public_html/modify.js
+++ app/public_html/modify.js
@@ -14,7 +14,7 @@ addButton.addEventListener("click", function() {
     }
     else if(addButton.className === "teams") {
         //get data from teams.html
-        //getForm();
+        getForm("teams");
     }
     else if(addButton.className === "games") {
         //get data from games.html
@@ -32,8 +32,14 @@ function submitForm(type) {
             pass: document.getElementById("psw").value
         };
     }
-    else if(type === "team") {
-
+    else if(type === "teams") {
+        data = {
+            type: "team",
+            name: document.getElementById("name").value,
+            player1: document.getElementById("p1").options[document.getElementById("p1").selectedIndex].value,
+            player2: document.getElementById("p2").options[document.getElementById("p2").selectedIndex].value,
+            player3: document.getElementById("p3").options[document.getElementById("p3").selectedIndex].value
+        };
     }
     console.log(data);
     document.getElementById("myForm").style.display = "none";
@@ -60,7 +66,42 @@ function getForm(type) {
     if(type ===  "player") {
         document.getElementById("myForm").style.display = "block";
     }
-    else if(type === "team") {
+    else if(type === "teams") {
+        let html = `<label><b>Player 1</b></label>
+        <select id="p1" onchange="populate(this.id,'p2')"><option value=""></option>`;
+        //list of all available players for first member
+        for(let i in test) {
+            html += `<option value=${test[i]}>${test[i]}</option>`;
+        }
+        html += `</select><label><b>Player 2</b></label><select id="p2" onchange="populate(this.id,'p3')"><option value=""></option>`;
+        let p1 = document.getElementById("p1");
+        //list of all remaining players for second member
+        for(let j in test) {
+            html += `<option value=${test[j]}>${test[j]}</option>`;
+        }
+        html += `</select><label><b>Player 3</b></label><select id="p3"><option value=""></option>`;
+        //list of all remaining players for third member
+        for(let l in test) {
+            html += `<option value=${test[l]}>${test[l]}</option>`;
+        }
+        html += `</select><button type="button" onclick="submitForm('teams')">Submit</button>`;
+        document.getElementById("teams").innerHTML += html;
+        document.getElementById("myForm").style.display = "block";
+    }
+}
 
+function populate(s1, s2) {
+    console.log("test");
+    let src1 = document.getElementById(s1);
+    let src2 = document.getElementById(s2);
+    src2.innerHTML = ``;
+    let index = test.indexOf(src1.value);
+    for(let i = 0; i < src1.options.length; i++) {
+        if(!(src1.selectedIndex === i)) {
+            let option = document.createElement("option");
+            option.value = src1.options[i].value;
+            option.innerHTML = src1.options[i].value;
+            src2.options.add(option);
+        }
     }
 }
