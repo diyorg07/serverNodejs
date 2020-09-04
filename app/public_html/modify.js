8d50c74b739a939a3ea0d49aa3e4d2b977078e6b
Kobe Uytiepo <kobeuyt@gmail.com>
2020-09-04 13:58:54 -0400

    Update modify.js

---
+++ app/public_html/modify.js
@@ -0,0 +1,105 @@
+let addButton = document.getElementById("add");
+let editButton = document.getElementById("edit");
+let delButton = document.getElementById("delete");
+
+addButton.addEventListener("click", function() {
+    let url = "";
+    let data = null;
+    if(addButton.className === "player") {
+        //get data from players.html
+        data = {};
+    }
+    else if(addButton.className === "teams") {
+        //get data from teams.html
+        data = {};
+    }
+    else if(addButton.className === "games") {
+        //get data from games.html
+        data = {};
+    }
+    fetch(url + `/add`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        if(response.status === 200) {
+            //DOM success message
+        }
+        else {
+            //DOM bad request message
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+})
+
+editButton.addEventListener("click", function() {
+    let url = "";
+    let data = null;
+    if(addButton.className === "player") {
+        //get data from players.html
+        data = {};
+    }
+    else if(addButton.className === "teams") {
+        //get data from teams.html
+        data = {};
+    }
+    else if(addButton.className === "games") {
+        //get data from games.html
+        data = {};
+    }
+    fetch(url + `/edit`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        if(response.status === 200) {
+            //DOM success message
+        }
+        else {
+            //DOM bad request message
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+})
+
+delButton.addEventListener("click", function() {
+    let url = "";
+    let data = null;
+    if(addButton.className === "player") {
+        //get data from players.html
+        data = {};
+    }
+    else if(addButton.className === "teams") {
+        //get data from teams.html
+        data = {};
+    }
+    else if(addButton.className === "games") {
+        //get data from games.html
+        data = {};
+    }
+    fetch(url + `/delete`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        if(response.status === 200) {
+            //DOM success message
+        }
+        else {
+            //DOM bad request message
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+})
