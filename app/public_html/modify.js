c40e90636d475e7fb8a714d9064ce9e6d506738e
Kobe Uytiepo <kobeuyt@gmail.com>
2020-09-04 14:40:54 -0400

    added form for getting new players

--- app/public_html/modify.js
+++ app/public_html/modify.js
@@ -7,10 +7,12 @@ addButton.addEventListener("click", function() {
     let data = null;
     if(addButton.className === "player") {
         //get data from players.html
+        getPlayerForm();
         data = {};
     }
     else if(addButton.className === "teams") {
         //get data from teams.html
+        getPlayerForm();
         data = {};
     }
     else if(addButton.className === "games") {
@@ -34,4 +36,33 @@ addButton.addEventListener("click", function() {
     }).catch(function (error) {
         console.log(error);
     });
-})
+});
+
+function submitForm() {
+    let container = document.getElementById("form");
+    container.innerHTML = "";
+}
+
+function getPlayerForm() {
+    let container = document.getElementById("form");
+    let html = `<div class="form-popup" id="myForm">
+                <form action="/action_page.php" class="form-container">
+                    <h1>New Player</h1>
+                
+                    <label><b>First Name</b></label>
+                    <input type="text" placeholder="Enter First Name" required>
+
+                    <label><b>Last Name</b></label>
+                    <input type="text" placeholder="Enter Last Name" required>
+
+                    <label><b>Email</b></label>
+                    <input type="text" placeholder="Enter Email" required>
+
+                    <label><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" required>
+                
+                    <button type="button" onclick="submitForm()">Submit</button>
+                </form>
+                </div>`;
+    container.innerHTML = html;
+}
