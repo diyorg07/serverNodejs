3ca8cb2dfe04079b59f9050239c750d124e95a23
Diyor Zakirov <diyorzakirov07@gmail.com>
2020-09-04 13:39:41 -0400

    connection to psql database

--- app/server.js
+++ app/server.js
@@ -1,16 +1,16 @@
-//const pg = require("pg");
+const pg = require("pg");
 const express = require("express");
 const app = express();
 
 const port = 3000;
 const hostname = "localhost";
 
-//const env = require("../env.json");
-//const Pool = pg.Pool;
-//const pool = new Pool(env);
-//pool.connect().then(function () {
-//    console.log(`Connected to database ${env.database}`);
-//});
+const env = require("../env.json");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+pool.connect().then(function () {
+   console.log(`Connected to database ${env.database}`);
+});
 
 app.use(express.static("public_html"));
 app.use(express.json());
